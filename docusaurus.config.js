431dc1ad18de31c7ccab9beeb5a980037f6796db
Jake (Chien) Le <chien.le@shopee.com>
2021-05-21 16:34:03 +0700

    Fix config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,13 +2,13 @@
 module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://pigbirdco.github.io/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'pigbirdco', // Usually your GitHub org/user name.
+  projectName: 'pigbirdco.github.io', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'My Site',
